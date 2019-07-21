ac93ae87466d90bb01ed5b7ed7897a7e813ff4b8
hodaia <hodaiarg@gmail.com>
2019-07-22 00:18:14 +0300

    change enum

---
+++ app/src/main/java/com/example/androidb/superquick/utils/EnumsUtils.java
@@ -0,0 +1,11 @@
+package com.example.androidb.superquick.utils;
+
+public class EnumsUtils {
+    public enum StatusRow {
+        width(1), height(2);
+
+        StatusRow(int i) {
+
+        }
+    }
+}
