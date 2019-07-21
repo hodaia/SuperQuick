5be9de33f89f6530f3ac87a91e1d75c567bfdb9b
hodaia <hodaiarg@gmail.com>
2019-07-22 00:42:28 +0300

    trying

--- app/src/main/java/com/example/androidb/superquick/activities/MapActivity.java
+++ app/src/main/java/com/example/androidb/superquick/activities/MapActivity.java
@@ -13,5 +13,6 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        int s;
     }
 }
